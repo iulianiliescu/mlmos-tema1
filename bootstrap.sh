05dbaf207d7fff346603be37fa042096489b4c52
iulianiliescu <iulian.alex.iliescu@gmail.com>
2019-03-09 10:48:54 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 
 yum -y update
 
-sed 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config > /etc/selinux/config
+sed 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config > /etc/selinux/config2
+sed 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config2 > /etc/selinux/config
