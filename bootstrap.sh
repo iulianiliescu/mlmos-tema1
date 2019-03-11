3dfcef453fc63e919446bb3992279a17306c3f08
Iulian <iulian.alex.iliescu@gmail.com>
2019-03-11 22:54:56 +0200

    network #4

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,10 +21,15 @@ static_ip=$(crudini --get $hostname.config general static_ip)
 gateway=$(crudini --get $hostname.config general gateway)
 dns=$(crudini --get $hostname.config general dns)
 
+echo "setting ip"
 nmcli con mod enp0s3 ipv4.addresses $static_ip/24
+echo "setting gateway"
 nmcli con mod enp0s3 ipv4.gateway $gateway
+echo "setting dns"
 nmcli con mod enp0s3 ipv4.dns $dns
+echo "setting manual"
 nmcli con mod enp0s3 ipv4.method manual
+echo "setting autoconnect"
 nmcli con mod enp0s3 connection.autoconnect yes
 
 exit 0
